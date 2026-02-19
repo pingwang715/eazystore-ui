44b6a1832378d6b4700fbf565960e86d2192e201
Ping Wang <pingwang715@gmail.com>
2026-02-19 15:52:49 +0100

    Create UI, use react route dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import Header from "./components/Header.jsx";
 import Footer from "./components/footer/Footer.jsx";
 import React from "react";
-import Home from "./components/Home.jsx";
+import { Outlet } from "react-router-dom";
+import { useNavigation } from "react-router-dom";
 
 function App() {
+  const navigation = useNavigation();
+
   return (
     <>
       <Header />
-      <Home />
+      {navigation.state === "loading" ? (
+        <div className="flex items-center justify-center min-h-[852px]">
+          <span className="text-4xl font-semibold text-primary dark:text-light">
+            Loading...
+          </span>
+        </div>
+      ) : (
+        <Outlet />
+      )}
       <Footer />
     </>
-
   );
 }
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,93 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faShoppingBasket, faTags } from "@fortawesome/free-solid-svg-icons";
+import {
+  faShoppingBasket,
+  faTags,
+  faSun,
+  faMoon,
+} from "@fortawesome/free-solid-svg-icons";
+import { useState, useEffect } from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
+  const [theme, setTheme] = useState(()=>{
+    return localStorage.getItem("theme") === "dark" ? "dark":"light";
+  })
 
-  const navLinkClass = "text-center text-lg font-primary font-semibold text-primary py-2";
+  useEffect(()=>{
+    if (theme === "dark") {
+        document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  },[theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme)=>{
+      const newTheme = prevTheme === "light" ? "dark" : "light";
+      localStorage.setItem("theme", newTheme);
+      return newTheme;
+    });
+  };
+
+  const navLinkClass =
+    "text-center text-lg font-primary font-semibold text-primary py-2 dark:text-light hover:text-dark dark:hover:text-lighter";
 
   return (
-    <header className="border-b border-gray-300 sticky top-0 z-20 bg-gray-100">
+    <header className="border-b border-gray-300 dark:border-gray-600 sticky top-0 z-20 bg-normalbg dark:bg-darkbg">
       <div className="flex items-center justify-between mx-auto max-w-[1152px] px-6 py-4">
-      <a href="/" className="text-center text-lg font-primary font-semibold text-primary py-2">
-        <FontAwesomeIcon icon={faTags} className="h-8 w-8" />
-        <span className="font-bold">Eazy Stickers</span>
-      </a>
+        <Link to="/" className={navLinkClass}>
+          <FontAwesomeIcon icon={faTags} className="h-8 w-8" />
+          <span className="font-bold"> Eazy Stickers</span>
+        </Link>
 
-      <nav className="flex items-center py-2 z-10">
-        <ul className="flex space-x-6">
-          <li>
-            <a href="/" className={navLinkClass}>Home</a>
-          </li>
-          <li>
-            <a href="/about" className={navLinkClass}>About</a>
-          </li>
-          <li>
-            <a href="/contact" className={navLinkClass}>Contact</a>
-          </li>
-          <li>
-            <a href="/login" className={navLinkClass}>Login</a>
-          </li>
-          <li>
-            <a href="/cart" className="text-primary py-2">
-            <FontAwesomeIcon icon={faShoppingBasket} />
-            </a>
-          </li>
-        </ul>
-      </nav>
+        <nav className="flex items-center py-2 z-10">
+          <button
+            className="flex items-center justify-center mx-3 w-8 h-8 rounded-full border border-primary dark:border-light transition duration-300 hover:bg-gray-300 dark:hover:bg-gray-600"
+            aria-label="Toggle theme"
+            onClick={toggleTheme}
+          >
+            <FontAwesomeIcon
+              icon={theme === "dark" ? faMoon : faSun}
+              className="w-4 h-4 dark:text-light text-primary"
+            />
+          </button>
+
+          <ul className="flex space-x-6">
+            <li>
+              <NavLink to="/home" className={({ isActive })=>
+                isActive ? `underline ${navLinkClass}` : navLinkClass
+              }>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/about" className={({ isActive })=>
+                isActive ? `underline ${navLinkClass}` : navLinkClass
+              }>
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact" className={({ isActive })=>
+                isActive ? `underline ${navLinkClass}` : navLinkClass
+              }>
+                Contact
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/login" className={({ isActive })=>
+                isActive ? `underline ${navLinkClass}` : navLinkClass
+              }>
+                Login
+              </NavLink>
+            </li>
+            <li>
+              <Link to="/cart" className="text-primary py-2">
+                <FontAwesomeIcon icon={faShoppingBasket} className="dark:text-light"/>
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     </header>
   );
